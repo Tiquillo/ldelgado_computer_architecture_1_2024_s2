8be67656f3f83d9bc563fa302098c4f17be3caf8
tiquillo <ldelgado@estudiantec.cr>
2024-10-22 10:40:46 -0600

    unión v1

--- VGA/pll_sim/aldec/rivierapro_setup.tcl
+++ VGA/pll_sim/aldec/rivierapro_setup.tcl
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 18.1 625 win32 2024.05.15.13:47:08
+# ACDS 22.1 915 win32 2024.10.22.10:22:07
 # ----------------------------------------
 # Auto-generated simulation script rivierapro_setup.tcl
 # ----------------------------------------
@@ -113,7 +113,7 @@ if ![info exists QSYS_SIMDIR] {
 }
 
 if ![info exists QUARTUS_INSTALL_DIR] { 
-  set QUARTUS_INSTALL_DIR "C:/intelfpga_lite/18.1/quartus/"
+  set QUARTUS_INSTALL_DIR "C:/intelfpga_lite/22.1std/quartus/"
 }
 
 if ![info exists USER_DEFINED_COMPILE_OPTIONS] { 
